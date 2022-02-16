f39bc86f01443d98b86bde56b9477568df80624b
Brian Vu <emailbrian.vu@gmail.com>
2022-02-16 15:31:03 -0800

    fix: updated example and fixed configure.sh script

--- src/scripts/configure.sh
+++ src/scripts/configure.sh
@@ -1,6 +1,7 @@
 PARAM_AWS_CLI_ACCESS_KEY_ID=$(eval echo "\$$PARAM_AWS_CLI_ACCESS_KEY_ID")
 PARAM_AWS_CLI_SECRET_ACCESS_KEY=$(eval echo "\$$PARAM_AWS_CLI_SECRET_ACCESS_KEY")
 PARAM_AWS_CLI_REGION=$(eval echo "\$$PARAM_AWS_CLI_REGION")
+PARAM_AWS_CLI_ROLE_ARN=$(eval echo "${PARAM_AWS_CLI_ROLE_ARN}")
 
 aws configure set aws_access_key_id \
     "$PARAM_AWS_CLI_ACCESS_KEY_ID" \
@@ -16,5 +17,10 @@ fi
 
 if [ "$PARAM_AWS_CLI_CONFIG_PROFILE_REGION" = "1" ]; then
     aws configure set region "$PARAM_AWS_CLI_REGION" \
+        --profile "$PARAM_AWS_CLI_PROFILE_NAME"
+fi
+
+if [ -n "$PARAM_AWS_CLI_ROLE_ARN" ]; then
+    aws configure set role_arn "$PARAM_AWS_CLI_ROLE_ARN" \
         --profile "$PARAM_AWS_CLI_PROFILE_NAME"
 fi
