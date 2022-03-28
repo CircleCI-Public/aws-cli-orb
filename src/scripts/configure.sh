9b2c851388f26faf23551c242d6be81f3ad7054b
Brian Vu <emailbrian.vu@gmail.com>
2022-03-27 23:10:11 -0700

    ci: final testing

--- src/scripts/configure.sh
+++ src/scripts/configure.sh
@@ -2,7 +2,9 @@ PARAM_AWS_CLI_ACCESS_KEY_ID=$(eval echo "\$$PARAM_AWS_CLI_ACCESS_KEY_ID")
 PARAM_AWS_CLI_SECRET_ACCESS_KEY=$(eval echo "\$$PARAM_AWS_CLI_SECRET_ACCESS_KEY")
 PARAM_AWS_CLI_REGION=$(eval echo "\$$PARAM_AWS_CLI_REGION")
 
-source "$BASH_ENV"
+if cat /etc/issue | grep "Alpine" || uname -a | grep "x86_64 Msys"; then
+    source "$BASH_ENV"
+fi
 echo "${PATH}"
 aws configure set aws_access_key_id \
     "$PARAM_AWS_CLI_ACCESS_KEY_ID" \
