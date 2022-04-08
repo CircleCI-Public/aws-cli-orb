2b1886556ecf915cf53a641ca9cd021b27492f93
Brian Vu <emailbrian.vu@gmail.com>
2022-04-08 09:14:21 -0700

    fix: deleted white space

--- src/scripts/assume-role-with-web-identity.sh
+++ src/scripts/assume-role-with-web-identity.sh
@@ -10,7 +10,7 @@ read -r AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN <<< \
 --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
 --output text)"
 
-export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID "
+export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
 export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
 export AWS_SESSION_TOKEN="$AWS_SESSION_TOKEN"
 
