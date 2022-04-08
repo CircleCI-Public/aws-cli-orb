4005acd45b5fa0ae5b1d0e9a83a88becf2d4e3b8
Brian Vu <emailbrian.vu@gmail.com>
2022-04-08 01:33:01 -0700

    test: debugging

--- src/scripts/assume-role-with-web-identity.sh
+++ src/scripts/assume-role-with-web-identity.sh
@@ -10,19 +10,15 @@ $(aws sts assume-role-with-web-identity \
 --duration-seconds "${PARAM_SESSION_DURATION}" \
 --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
 --output text)
+# aws sts get-caller-identity
 
 echo "this works" >> test.txt
 
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID 
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
-aws sts get-caller-identity
 
 echo "AKI ${AWS_ACCESS_KEY_ID}" >> test.txt
 echo "SAC ${AWS_SECRET_ACCESS_KEY}" >> test.txt
 echo "ST ${AWS_SESSION_TOKEN}" >> test.txt
 
-
-# aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile "B-VU"
-# aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile "B-VU"
-# aws configure set aws_session_token $AWS_SESSION_TOKEN --profile "B-VU"
