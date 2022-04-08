2d2812694360239bb02581c7ca1a071541fd07e8
Brian Vu <emailbrian.vu@gmail.com>
2022-04-08 01:24:56 -0700

    test: debugging

--- src/scripts/assume-role-with-web-identity.sh
+++ src/scripts/assume-role-with-web-identity.sh
@@ -1,6 +1,6 @@
 PARAM_ROLE_SESSION_NAME=$(eval echo "\$$PARAM_ROLE_SESSION_NAME")
 
-echo "${PARAM_ROLE_SESSION_NAME}"
+echo "${PARAM_ROLE_SESSION_NAME}" >> test.txt
 
 read -r AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN <<< \
 $(aws sts assume-role-with-web-identity \
@@ -11,14 +11,16 @@ $(aws sts assume-role-with-web-identity \
 --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
 --output text)
 
-export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+echo "this works" >> test.txt
+
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID 
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
 aws sts get-caller-identity
 
-echo "AKI ${AWS_ACCESS_KEY_ID}"
-echo "SAC ${AWS_SECRET_ACCESS_KEY}"
-echo "ST ${AWS_SESSION_TOKEN}"
+echo "AKI ${AWS_ACCESS_KEY_ID}" >> test.txt
+echo "SAC ${AWS_SECRET_ACCESS_KEY}" >> test.txt
+echo "ST ${AWS_SESSION_TOKEN}" >> test.txt
 
 
 # aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile "B-VU"
