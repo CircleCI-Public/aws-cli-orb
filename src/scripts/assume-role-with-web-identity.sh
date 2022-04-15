cdf94d29047d79bb23abf4a67775eaa6e08bb2e1
Brian Vu <emailbrian.vu@gmail.com>
2022-04-15 12:43:40 -0700

    fix: testing

--- src/scripts/assume-role-with-web-identity.sh
+++ src/scripts/assume-role-with-web-identity.sh
@@ -10,10 +10,11 @@ read -r AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN <<< \
 --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
 --output text)"
 
+aws sts get-caller-identity
+
 echo "export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" >> "$BASH_ENV"
 echo "export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> "$BASH_ENV"
 echo "export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN" >> "$BASH_ENV"
 
-aws sts get-caller-identity
 
 
