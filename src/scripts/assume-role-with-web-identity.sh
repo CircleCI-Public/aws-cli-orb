236a3475272ceaa37ea123b4828a17d932f9ea8e
Brian Vu <emailbrian.vu@gmail.com>
2022-04-19 11:21:48 -0700

    fix: added shellcheck ignore

--- src/scripts/assume-role-with-web-identity.sh
+++ src/scripts/assume-role-with-web-identity.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 PARAM_ROLE_SESSION_NAME=$(eval echo "${PARAM_ROLE_SESSION_NAME}")
 
-# shellcheck disable=SC2086
-read -r AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN <<< \
-    "$(aws sts assume-role-with-web-identity \
+# shellcheck disable=SC2086,SC2034
+read -r AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN <<< "$(aws sts assume-role-with-web-identity \
     --role-arn ${PARAM_AWS_CLI_ROLE_ARN} \
     --role-session-name ${PARAM_ROLE_SESSION_NAME} \
     --web-identity-token ${CIRCLE_OIDC_TOKEN} \
@@ -11,7 +10,6 @@ read -r AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN <<< \
     --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
     --output text)"
 
-    
 echo 'export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN' >> "$BASH_ENV" 
 
 aws sts get-caller-identity
