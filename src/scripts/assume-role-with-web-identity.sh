58d57db8dc077b7b2d6a9e7b9d690b36ed356628
Brian Vu <64455338+brivu@users.noreply.github.com>
2022-04-21 12:05:41 -0700

    feat:v3.1.0 (#103)
    * rerun
    
    * fix: removed deprecated parameter from script
    
    * fix: added sudo valdation for Alpine
    
    * fix: removed redundant if statement
    
    * feat: added usage example for web identity
    
    * fix: added shellcheck exclusions
    
    * fix: added web identity parameters to test job
    
    * ci: added testing for assuming web identity
    
    * fix: added context to jobs
    
    * feat: added assume-role-with-web-identity-command
    
    * fix: added assume-role-with-web-identity parameters and logic
    
    * fix: renamed usage example
    
    * fix: added renamed usage example
    
    * fix: added assume-role-with-web-identity.sh
    
    * chore: code cleanup
    
    * fix: fixed typo
    
    * fix: added validation to configure session token
    
    * test: removing to shellcheck rules

---
+++ src/scripts/assume-role-with-web-identity.sh
@@ -0,0 +1,15 @@
+PARAM_ROLE_SESSION_NAME=$(eval echo "${PARAM_ROLE_SESSION_NAME}")
+
+# shellcheck disable=SC2086,SC2034
+read -r AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN <<<"$(aws sts assume-role-with-web-identity \
+    --role-arn ${PARAM_AWS_CLI_ROLE_ARN} \
+    --role-session-name ${PARAM_ROLE_SESSION_NAME} \
+    --web-identity-token ${CIRCLE_OIDC_TOKEN} \
+    --duration-seconds ${PARAM_SESSION_DURATION} \
+    --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
+    --output text)"
+{
+    echo "export AWS_ACCESS_KEY_ID=\"${AWS_ACCESS_KEY_ID}\""
+    echo "export AWS_SESSION_TOKEN=\"${AWS_SESSION_TOKEN}\""
+    echo "export AWS_SECRET_ACCESS_KEY=\"${AWS_SECRET_ACCESS_KEY}\""
+} >>"$BASH_ENV"
--- src/scripts/configure.sh
+++ src/scripts/configure.sh
@@ -1,18 +1,20 @@
 PARAM_AWS_CLI_ACCESS_KEY_ID=$(eval echo "\$$PARAM_AWS_CLI_ACCESS_KEY_ID")
 PARAM_AWS_CLI_SECRET_ACCESS_KEY=$(eval echo "\$$PARAM_AWS_CLI_SECRET_ACCESS_KEY")
 PARAM_AWS_CLI_REGION=$(eval echo "\$$PARAM_AWS_CLI_REGION")
-PARAM_AWS_CLI_ROLE_ARN=$(eval echo "${PARAM_AWS_CLI_ROLE_ARN}")
 
-if cat /etc/issue | grep "Alpine" || uname -a | grep "x86_64 Msys"; then
-    source "$BASH_ENV"
-fi
-echo "${PATH}"
 aws configure set aws_access_key_id \
     "$PARAM_AWS_CLI_ACCESS_KEY_ID" \
     --profile "$PARAM_AWS_CLI_PROFILE_NAME"
+
 aws configure set aws_secret_access_key \
     "$PARAM_AWS_CLI_SECRET_ACCESS_KEY" \
     --profile "$PARAM_AWS_CLI_PROFILE_NAME"
+
+if [ -n "${AWS_SESSION_TOKEN}" ]; then
+    aws configure set aws_session_token \
+        "${AWS_SESSION_TOKEN}" \
+        --profile "$PARAM_AWS_CLI_PROFILE_NAME"
+fi
 
 if [ "$PARAM_AWS_CLI_CONFIG_DEFAULT_REGION" = "1" ]; then
     aws configure set default.region "$PARAM_AWS_CLI_REGION" \
