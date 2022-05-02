5e7fbacb718b5e0767a7a5ebbcee5fd28951e55d
Brian Vu <emailbrian.vu@gmail.com>
2022-05-02 15:31:52 -0700

    test: testing validation for web identity assumption

--- src/scripts/assume-role-with-web-identity.sh
+++ src/scripts/assume-role-with-web-identity.sh
@@ -5,15 +5,14 @@ PARAM_ROLE_SESSION_NAME=$(eval echo "${PARAM_ROLE_SESSION_NAME}")
 #     exit 1
 # fi
 # shellcheck disable=SC2034
-AWS_STS_COMMAND=$(aws sts assume-role-with-web-identity \
-    --role-arn "${PARAM_AWS_CLI_ROLE_ARN}" \
-    --role-session-name "${PARAM_ROLE_SESSION_NAME}" \
-    --web-identity-token "${CIRCLE_OIDC_TOKEN}" \
-    --duration-seconds "${PARAM_SESSION_DURATION}" \
-    --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
-    --output text; exit "${PIPESTATUS[0]}")
+# AWS_STS_COMMAND=$(aws sts assume-role-with-web-identity \
+#     --role-arn "${PARAM_AWS_CLI_ROLE_ARN}" \
+#     --role-session-name "${PARAM_ROLE_SESSION_NAME}" \
+#     --web-identity-token "${CIRCLE_OIDC_TOKEN}" \
+#     --duration-seconds "${PARAM_SESSION_DURATION}" \
+#     --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
+#     --output text; exit "${PIPESTATUS[0]}")
 
-if echo $? | grep 0 ; then
     # shellcheck disable=SC2086,SC2034
     read -r AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN <<<"$(aws sts assume-role-with-web-identity \
         --role-arn ${PARAM_AWS_CLI_ROLE_ARN} \
@@ -21,7 +20,8 @@ if echo $? | grep 0 ; then
         --web-identity-token ${CIRCLE_OIDC_TOKEN} \
         --duration-seconds ${PARAM_SESSION_DURATION} \
         --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
-        --output text)"
+        --output text; exit "${PIPESTATUS[0]}")"
+if echo $? | grep 0 ; then
     {
         echo "export AWS_ACCESS_KEY_ID=\"${AWS_ACCESS_KEY_ID}\""
         echo "export AWS_SESSION_TOKEN=\"${AWS_SESSION_TOKEN}\""
