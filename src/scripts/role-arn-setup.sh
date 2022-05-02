e2b169b0ec8e2eca1a933dad534992119a31db83
Brian Vu <emailbrian.vu@gmail.com>
2022-05-02 11:18:44 -0700

    fix: eval parameter

--- src/scripts/role-arn-setup.sh
+++ src/scripts/role-arn-setup.sh
@@ -1,7 +1,8 @@
+PARAM_AWS_CLI_ROLE_ARN=$(eval echo "${PARAM_AWS_CLI_ROLE_ARN}")
 if [ ! -f "${HOME}/.aws/credentials" ]; then
     echo "Credentials not found. Run setup command before role-arn-setup."
     exit 1
 fi
 
-aws configure set profile."${PARAM_AWS_CLI_PROFILE_NAME}".role_arn "${!PARAM_AWS_CLI_ROLE_ARN}"
+aws configure set profile."${PARAM_AWS_CLI_PROFILE_NAME}".role_arn "${PARAM_AWS_CLI_ROLE_ARN}"
 aws configure set profile."${PARAM_AWS_CLI_PROFILE_NAME}".source_profile "${PARAM_AWS_CLI_SOURCE_PROFILE}"
