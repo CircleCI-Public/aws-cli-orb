77c88a726c13ccaff5a1452a63eec04ffe7fb3a8
Brian Vu <emailbrian.vu@gmail.com>
2022-08-25 14:45:45 -0700

    fix: added validation for missing keys

--- src/scripts/configure.sh
+++ src/scripts/configure.sh
@@ -3,15 +3,14 @@ if cat /etc/issue | grep "Alpine" >/dev/null 2>&1; then
     . $BASH_ENV
 fi
 
-{ 
-    echo "$PARAM_AWS_CLI_ACCESS_KEY_ID"
-}
 PARAM_AWS_CLI_ACCESS_KEY_ID=$(eval echo "\$$PARAM_AWS_CLI_ACCESS_KEY_ID")
 PARAM_AWS_CLI_SECRET_ACCESS_KEY=$(eval echo "\$$PARAM_AWS_CLI_SECRET_ACCESS_KEY")
 PARAM_AWS_CLI_REGION=$(eval echo "\$$PARAM_AWS_CLI_REGION")
-{ 
-    echo "$PARAM_AWS_CLI_ACCESS_KEY_ID"
-}
+
+if [ -z "$PARAM_AWS_CLI_ACCESS_KEY_ID" ] || [ -z "${PARAM_AWS_CLI_SECRET_ACCESS_KEY}" ]; then 
+    echo "Cannot configure profile. AWS access keu id and AWS secret access key must be provided"
+    exit 1
+fi
 
 aws configure set aws_access_key_id \
     "$PARAM_AWS_CLI_ACCESS_KEY_ID" \
