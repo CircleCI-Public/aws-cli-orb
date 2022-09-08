cab9ae97da3d8ad5857c1911e421bde70cf36653
David Tolman <dajoto1@gmail.com>
2022-09-08 08:40:39 +0100

    feat: allow env variable eval in profile-name
    By allowing eval of the profile-name param/env_var inside
    role-arn-setup command, we can allow other orbs to consume this orb
    where profile-name might be supplied via context

--- src/scripts/role-arn-setup.sh
+++ src/scripts/role-arn-setup.sh
@@ -1,8 +1,12 @@
-PARAM_AWS_CLI_ROLE_ARN=$(eval echo "${PARAM_AWS_CLI_ROLE_ARN}")
+#!/usr/bin/env sh
+
 if [ ! -f "${HOME}/.aws/credentials" ]; then
     echo "Credentials not found. Run setup command before role-arn-setup."
     exit 1
 fi
+
+PARAM_AWS_CLI_PROFILE_NAME=$(eval echo "${PARAM_AWS_CLI_PROFILE_NAME}")
+PARAM_AWS_CLI_ROLE_ARN=$(eval echo "${PARAM_AWS_CLI_ROLE_ARN}")
 
 aws configure set profile."${PARAM_AWS_CLI_PROFILE_NAME}".role_arn "${PARAM_AWS_CLI_ROLE_ARN}"
 aws configure set profile."${PARAM_AWS_CLI_PROFILE_NAME}".source_profile "${PARAM_AWS_CLI_SOURCE_PROFILE}"
