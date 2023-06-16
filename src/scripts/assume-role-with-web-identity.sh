a27c02c95b1125eab1d423099da4b8a2b4bc328d
David Vasandani <david@vasandani.me>
2023-06-16 13:47:04 -0700

    feat: add retry and random sleep (#137)
    * feat: add retry loop and sleep
    
    * revert source_url back to CircleCI-Public

--- src/scripts/assume-role-with-web-identity.sh
+++ src/scripts/assume-role-with-web-identity.sh
@@ -16,24 +16,30 @@ if [ ! "$(command -v aws)" ]; then
     exit 1
 fi
 
-read -r AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN <<EOF
-$(aws sts assume-role-with-web-identity \
---role-arn "${PARAM_AWS_CLI_ROLE_ARN}" \
---role-session-name "${PARAM_ROLE_SESSION_NAME}" \
---web-identity-token "${CIRCLE_OIDC_TOKEN_V2}" \
---duration-seconds "${PARAM_SESSION_DURATION}" \
---query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
---output text)
+for i in {0..3}; do
+    sleep $((i * 2))
+    read -r AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN <<EOF
+    $(aws sts assume-role-with-web-identity \
+        --role-arn "${PARAM_AWS_CLI_ROLE_ARN}" \
+        --role-session-name "${PARAM_ROLE_SESSION_NAME}" \
+        --web-identity-token "${CIRCLE_OIDC_TOKEN_V2}" \
+        --duration-seconds "${PARAM_SESSION_DURATION}" \
+        --query 'Credentials.[AccessKeyId,SecretAccessKey,SessionToken]' \
+        --output text)
 EOF
 
-if [ -z "${AWS_ACCESS_KEY_ID}" ] || [ -z "${AWS_SECRET_ACCESS_KEY}" ] || [ -z "${AWS_SESSION_TOKEN}" ]; then
-    echo "Failed to assume role";
-    exit 1
-else 
-    {
-        echo "export AWS_ACCESS_KEY_ID=\"${AWS_ACCESS_KEY_ID}\""
-        echo "export AWS_SECRET_ACCESS_KEY=\"${AWS_SECRET_ACCESS_KEY}\""
-        echo "export AWS_SESSION_TOKEN=\"${AWS_SESSION_TOKEN}\""
-    } >>"$BASH_ENV"
-    echo "Assume role with web identity succeeded"
-fi
+    if [ -z "${AWS_ACCESS_KEY_ID}" ] || [ -z "${AWS_SECRET_ACCESS_KEY}" ] || [ -z "${AWS_SESSION_TOKEN}" ]; then
+        echo "Failed to assume role on attempt $i"
+        if [ "$i" -eq 3 ]; then
+            exit 1
+        fi
+    else
+        {
+            echo "export AWS_ACCESS_KEY_ID=\"${AWS_ACCESS_KEY_ID}\""
+            echo "export AWS_SECRET_ACCESS_KEY=\"${AWS_SECRET_ACCESS_KEY}\""
+            echo "export AWS_SESSION_TOKEN=\"${AWS_SESSION_TOKEN}\""
+        } >>"$BASH_ENV"
+        echo "Assume role with web identity succeeded"
+        break
+    fi
+done
