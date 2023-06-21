67a1dd50bf0c160bae1c7ffb8300aa1af99193b8
Brian Vu <64455338+brivu@users.noreply.github.com>
2023-06-21 15:02:23 -0700

    chore: update src/scripts/role_arn_setup.sh
    Co-authored-by: Eric Ribeiro <ericribeiro@outlook.com.br>

--- src/scripts/role_arn_setup.sh
+++ src/scripts/role_arn_setup.sh
@@ -1,6 +1,6 @@
 # shellcheck disable=SC2148
-ORB_STR_ROLE_ARN=$(circleci env subst "${ORB_STR_ROLE_ARN}")
-ORB_STR_PROFILE_NAME=$(circleci env subst "${ORB_STR_PROFILE_NAME}")
+ORB_STR_ROLE_ARN="$(circleci env subst "${ORB_STR_ROLE_ARN}")"
+ORB_STR_PROFILE_NAME="$(circleci env subst "${ORB_STR_PROFILE_NAME}")"
 ORB_EVAL_SOURCE_PROFILE=$(circleci env subst "${ORB_EVAL_SOURCE_PROFILE}")
 if [ ! -f "${HOME}/.aws/credentials" ]; then
     echo "Credentials not found. Run setup command before role-arn-setup."
