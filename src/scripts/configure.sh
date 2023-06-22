10dada9e6f526a803d142f885760ccb10ac375d7
Brian Vu <emailbrian.vu@gmail.com>
2023-06-22 14:12:35 -0700

    refactor: change variable type

--- src/scripts/configure.sh
+++ src/scripts/configure.sh
@@ -5,23 +5,24 @@ if grep "Alpine" /etc/issue > /dev/null 2>&1; then
     . "$BASH_ENV"
 fi
 
-ORB_ENV_ACCESS_KEY_ID=$(circleci env subst "\$$ORB_ENV_ACCESS_KEY_ID")
-ORB_ENV_SECRET_ACCESS_KEY=$(circleci env subst "\$$ORB_ENV_SECRET_ACCESS_KEY")
+ORB_STR_ACCESS_KEY_ID=$(circleci env subst "$ORB_STR_ACCESS_KEY_ID")
+ORB_STR_SECRET_ACCESS_KEY=$(circleci env subst "$ORB_STR_SECRET_ACCESS_KEY")
 AWS_SESSION_TOKEN="$(circleci env subst "$AWS_SESSION_TOKEN")"
-ORB_STR_REGION="$(circleci env subst "\$$ORB_STR_REGION")"
+ORB_STR_REGION="$(circleci env subst "$ORB_STR_REGION")"
 ORB_STR_PROFILE_NAME="$(circleci env subst "$ORB_STR_PROFILE_NAME")"
 
-if [ -z "$ORB_ENV_ACCESS_KEY_ID" ] || [ -z "${ORB_ENV_SECRET_ACCESS_KEY}" ]; then 
+if [ -z "$ORB_STR_ACCESS_KEY_ID" ] || [ -z "${ORB_STR_SECRET_ACCESS_KEY}" ]; then 
     echo "Cannot configure profile. AWS access key id and AWS secret access key must be provided."
     exit 1
 fi
 
+set -x
 aws configure set aws_access_key_id \
-    "$ORB_ENV_ACCESS_KEY_ID" \
+    "$ORB_STR_ACCESS_KEY_ID" \
     --profile "$ORB_STR_PROFILE_NAME"
 
 aws configure set aws_secret_access_key \
-    "$ORB_ENV_SECRET_ACCESS_KEY" \
+    "$ORB_STR_SECRET_ACCESS_KEY" \
     --profile "$ORB_STR_PROFILE_NAME"
 
 if [ -n "${AWS_SESSION_TOKEN}" ]; then
@@ -31,11 +32,11 @@ if [ -n "${AWS_SESSION_TOKEN}" ]; then
 fi
 
 if [ "$ORB_BOOL_CONFIG_DEFAULT_REGION" = "1" ]; then
-    aws configure set default.region "$ORB_STR_REGION" \
-        --profile "$ORB_STR_PROFILE_NAME"
+    aws configure set default.region "$ORB_STR_REGION"
 fi
 
 if [ "$ORB_BOOL_CONFIG_PROFILE_REGION" = "1" ]; then
     aws configure set region "$ORB_STR_REGION" \
         --profile "$ORB_STR_PROFILE_NAME"
 fi
+set +x
