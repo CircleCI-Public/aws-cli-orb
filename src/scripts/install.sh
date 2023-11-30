05ae7a99b2f7148790fd7d8da011f9e3b3d76e4e
Brian Vu <emailbrian.vu@gmail.com>
2023-11-29 20:43:19 -0800

    fix: add validation to check for specified version

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -34,6 +34,9 @@ Toggle_Pager(){
 if ! command -v aws >/dev/null 2>&1; then
     Install_AWS_CLI "${AWS_CLI_STR_AWS_CLI_VERSION}"
     Toggle_Pager
+elif aws --version | grep "${AWS_CLI_STR_AWS_CLI_VERSION}"; then
+    echo "AWS CLI version ${AWS_CLI_STR_AWS_CLI_VERSION} already installed. Skipping installation"
+    exit 0
 elif [ "$AWS_CLI_BOOL_OVERRIDE" -eq 1 ] || [ "${AWS_CLI_STR_AWS_CLI_VERSION}" != "latest" ]; then
     Uninstall_AWS_CLI
     Install_AWS_CLI "${AWS_CLI_STR_AWS_CLI_VERSION}"
