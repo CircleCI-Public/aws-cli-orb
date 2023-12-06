5a92b1f34a4744f2cd5816e2a7655cc7ee131cbd
Brian Vu <64455338+brivu@users.noreply.github.com>
2023-12-06 00:12:55 -0800

    refactor: skip installation if specified version is installed (#182)
    * ci: add test to bypass installation if version matches
    
    * fix: add validation to check for specified version
    
    * fix: remove alias
    
    * fix: edit executor
    
    * fix: address pr comments

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -33,6 +33,9 @@ Toggle_Pager(){
 
 if ! command -v aws >/dev/null 2>&1; then
     Install_AWS_CLI "${AWS_CLI_STR_AWS_CLI_VERSION}"
+elif aws --version | awk '{print $2}' |grep "${AWS_CLI_STR_AWS_CLI_VERSION}"; then
+    echo "AWS CLI version ${AWS_CLI_STR_AWS_CLI_VERSION} already installed. Skipping installation"
+    exit 0
 elif [ "$AWS_CLI_BOOL_OVERRIDE" -eq 1 ] || [ "${AWS_CLI_STR_AWS_CLI_VERSION}" != "latest" ]; then
     Uninstall_AWS_CLI
     Install_AWS_CLI "${AWS_CLI_STR_AWS_CLI_VERSION}"
