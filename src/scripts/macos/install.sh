670b1173c46dd26374c2a1d9e11ec6ad3eef1576
marboledacci <mateo.arboleda@circleci.com>
2024-08-21 13:53:15 -0500

    Add a way to install cli using brew on mac

--- src/scripts/macos/install.sh
+++ src/scripts/macos/install.sh
@@ -1,20 +1,29 @@
 #!/bin/sh
 Install_AWS_CLI() {
-    if [ "$1" = "latest" ]; then
-        version=""
-    else
-        version="-$1"
-    fi
     echo "Installing AWS CLI v$version"
-    cd /tmp || exit
-    curl -o awscli.tar.gz "https://awscli.amazonaws.com/awscli$version.tar.gz"
-    mkdir awscli
-    tar -xzf awscli.tar.gz -C awscli --strip-components=1
-    rm awscli.tar.gz
-    cd awscli || exit
-    ./configure --with-download-deps
-    make
-    $SUDO make install
+    if [ "$USE_BREW" -eq 1 ]; then
+        if [ "$1" = "latest" ]; then
+            version=""
+        else
+            version="@$1"
+        fi
+        brew install awscli$version
+    else
+        if [ "$1" = "latest" ]; then
+            version=""
+        else
+            version="-$1"
+        fi
+        cd /tmp || exit
+        curl -o awscli.tar.gz "https://awscli.amazonaws.com/awscli$version.tar.gz"
+        mkdir awscli
+        tar -xzf awscli.tar.gz -C awscli --strip-components=1
+        rm awscli.tar.gz
+        cd awscli || exit
+        ./configure --with-download-deps
+        make
+        $SUDO make install
+    fi
 }
 
 Uninstall_AWS_CLI() {
