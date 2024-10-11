51996a179f32dc0102b20491eb83ece96109d3b0
marboledacci <mateo.arboleda@circleci.com>
2024-10-11 08:32:35 -0500

    fix: Multiple setups not doing the right configuration (#207)
    * Confiure a session token only when role_arn is provided
    
    * Add new tests to validate multiple sequential setups
    
    * Add debug
    
    * Extra debug
    
    * Extra debug
    
    * Add a different debug
    
    * Add a different debug
    
    * Add set_aws_env_vars false in first multiple setups job test
    
    * Remove debug
    
    * Revert fix to see if it works with the tests updated
    
    * Update validation to know which env to use
    
    * Add extra tests
    
    * Fix alias

--- src/scripts/configure.sh
+++ src/scripts/configure.sh
@@ -6,15 +6,15 @@ AWS_CLI_STR_SESSION_TOKEN="$(echo "$AWS_CLI_STR_SESSION_TOKEN" | circleci env su
 AWS_CLI_STR_REGION="$(echo "$AWS_CLI_STR_REGION" | circleci env subst)"
 AWS_CLI_STR_PROFILE_NAME="$(echo "$AWS_CLI_STR_PROFILE_NAME" | circleci env subst)"
 AWS_CLI_BOOL_SET_AWS_ENV_VARS="$(echo "$AWS_CLI_BOOL_SET_AWS_ENV_VARS" | circleci env subst)"
+AWS_CLI_STR_ROLE_ARN="$(echo "${AWS_CLI_STR_ROLE_ARN}" | circleci env subst)"
 
-if [ -z "$AWS_CLI_STR_ACCESS_KEY_ID" ] && [ -z "${AWS_CLI_STR_SECRET_ACCESS_KEY}" ] && [ "$AWS_CLI_BOOL_SET_AWS_ENV_VARS" = 0 ]; then 
+if [ "$AWS_CLI_BOOL_SET_AWS_ENV_VARS" = 0 ]; then 
     temp_file="/tmp/${AWS_CLI_STR_PROFILE_NAME}.keys"
     . "$temp_file"
 else 
     touch "${BASH_ENV}"
     . "${BASH_ENV}"
 fi
-
 aws configure set aws_access_key_id \
     "$AWS_CLI_STR_ACCESS_KEY_ID" \
     --profile "$AWS_CLI_STR_PROFILE_NAME"
@@ -23,7 +23,7 @@ aws configure set aws_secret_access_key \
     "$AWS_CLI_STR_SECRET_ACCESS_KEY" \
     --profile "$AWS_CLI_STR_PROFILE_NAME"
 
-if [ -n "${AWS_CLI_STR_SESSION_TOKEN}" ]; then
+if [ -n "${AWS_CLI_STR_ROLE_ARN}" ]; then
     aws configure set aws_session_token \
         "${AWS_CLI_STR_SESSION_TOKEN}" \
         --profile "$AWS_CLI_STR_PROFILE_NAME"
